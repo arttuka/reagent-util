8111d3af65379d6b583f4f15432a8923ca0ed781
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2020-02-08 08:00:11 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject reagent-util "0.1.0-SNAPSHOT"
+  :description "Reagent utilities for my projects"
+  :license {:name "Eclipse Public License 2.0"
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.597"]
+                 [reagent "0.9.1"]
+                 [arttuka/reagent-material-ui "4.8.3-0"]]
+  :plugins [[lein-cljfmt "0.6.6"]]
+  :source-paths ["src/cljs"])
