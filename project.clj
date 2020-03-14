44bcd2128f1e06b75420df54373f73a73da95384
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2020-03-14 17:31:52 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.597"]
-                 [reagent "0.9.1"]
-                 [arttuka/reagent-material-ui "4.8.3-0"]]
+                 [reagent "0.10.0"]
+                 [arttuka/reagent-material-ui "4.9.5-1"]]
   :plugins [[lein-cljfmt "0.6.6"]]
   :source-paths ["src/cljs"])
